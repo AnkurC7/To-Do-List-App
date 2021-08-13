02617db913c64662de41a9d620490d2ec6ff6ca1
Ankur Chaulagain <ankurchaulagain@gmail.com>
2021-08-13 22:45:24 +0545

    Delete fixes

--- src/App.js
+++ src/App.js
@@ -32,14 +32,19 @@ function App() {
   ]);
 
   const deleteTask = (id) => {
-    setTasks(tasks.filter(() => tasks.id !== id));
+    setTasks(tasks.filter((task) => task.id !== id));
+
     //only those that return true will be filtered
   };
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
