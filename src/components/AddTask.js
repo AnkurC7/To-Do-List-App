b0b3844686db5d9676f84c31c376ab1ade648411
Ankur Chaulagain <ankurchaulagain@gmail.com>
2021-08-14 15:07:29 +0545

    Typos fix

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -50,7 +50,7 @@ const AddTask = ({ onAdd }) => {
           onChange={(e) => setReminder(e.currentTarget.checked)}
         />
       </div>
-      <input type="submit" value="Save Taskk" className="btn btn-block" />
+      <input type="submit" value="Save Task" className="btn btn-block" />
     </form>
   );
 };
