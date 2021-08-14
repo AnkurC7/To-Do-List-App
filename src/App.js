361276d110c352d9350a3310c0dcaa9c1d2ef92a
Ankur Chaulagain <ankurchaulagain@gmail.com>
2021-08-14 11:12:37 +0545

    Added reminder fuctionality

--- src/App.js
+++ src/App.js
@@ -37,11 +37,19 @@ function App() {
     //only those that return true will be filtered
   };
 
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To Show"
       )}
