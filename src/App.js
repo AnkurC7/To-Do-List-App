2fda2208a98dc3a7f2c9c06d59d2f5412d1f179a
Ankur Chaulagain <ankurchaulagain@gmail.com>
2021-08-14 14:51:30 +0545

    Add tasks and other logic completion

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -31,6 +33,12 @@ function App() {
     },
   ]);
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task }; //{id: idvalue} try doing just any number only to understand, task passed is an object too
+    setTasks([...tasks, newTask]);
+  };
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
 
@@ -47,7 +55,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask ? <AddTask onAdd={addTask} /> : ""}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
---
+++ src/components/AddTask.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    /* Text is an empty string initially. Empty string means false in JS. So !text=not false=true. If there is text then it is true so !text=not true=false so there's no alerts */
+    // console.log({ text })
+    // console.log(text)
+
+    if (!text) {
+      alert("Please add a task");
+      return;
+    }
+    onAdd({ text, day, reminder });
+    setText("");
+    setDay("");
+    setReminder(false);
+  };
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input
+          type="text"
+          placeholder="Add Day & Time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder} //t/f and checked accordingly
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
+      </div>
+      <input type="submit" value="Save Taskk" className="btn btn-block" />
+    </form>
+  );
+};
+
+export default AddTask;
